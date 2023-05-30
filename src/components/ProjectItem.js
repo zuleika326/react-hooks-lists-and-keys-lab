c667a7f9565aaa88c9d8883bd964881d1f76be05
zuleika <njokizuleika@gmail.com>
2023-05-30 15:42:55 +0300

    adds my project

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,11 +3,13 @@ import React from "react";
 function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
-      <h3>{name}</h3>
+      <div>
+      <h2>{name}</h2>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      {technologies.map((tech) => (
+        <span key={tech}>{tech}</span>
+      ))}
+    </div>
     </div>
   );
 }
