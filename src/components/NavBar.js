6c06b094f0d289f4b4bd2289b3f2ff2e29d825c2
zuleika <njokizuleika@gmail.com>
2023-05-30 14:14:45 +0300

    adds a navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+    <>
+     <a href="#home">home</a>
+     <a href="#About">About</a>
+     <a href="#Projects">projects</a>
+    </>
+    }</nav>;
 }
 
 export default NavBar;
+
